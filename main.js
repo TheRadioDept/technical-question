1f98e8e4d678b1582d1f5f23ab6d252d9976382a
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-03-24 23:43:48 +0400

    Added .JS script
    Script for printing every country's official name from .json file using translation key as a parameter.

---
+++ main.js
@@ -0,0 +1,29 @@
+const data = require('/home/farkhad/progs/JS/countries.json'); /* Passing a .json file into javascript*/
+ 
+ var keys = ['cym','deu','fra','hrv','ita','jpn','nld','por','rus','spa'];  /* creating an array of translation keys */
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+    rl.question('Please enter the translation key ', (key) => {
+        if (keys.includes(key)){
+            console.log(data.map(point=>point.translations.key));
+        }
+    else {
+        console.log("Translation key is not supported. ");
+    }
+  rl.close();
+});
+
+/*if(keys.includes(${answer})){ 
+    
+    console.log(data.map(point=>point.translations.deu.official));  /* printing official name of the country depending on the translation key inserted 
+}
+else{ 
+    console.log("Translation key is not supported");
+} 
+*/
