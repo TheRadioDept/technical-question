bddefe4d9bcf143527a2c5f655db023ddc28c2e4
Farkhad <farkhadmaratov99@gmail.com>
2022-03-25 17:51:06 +0400

    Update main.js
    Added `.official` to return only official names of the coutries.

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-const data = require('/home/farkhad/progs/JS/countries.json'); /* Passing a .json file into javascript*/
+const data = require('/home/farkhad/progs/JS/countries.json');          /* Passing .json file into javascript*/
 
-var keys = ['cym','deu','fra','hrv','ita','jpn','nld','por','rus','spa'];  /* creating an array of translation keys */
-const keys1 = keys.toString().toLowerCase();
+var keys = ['cym', 'deu', 'fra', 'hrv', 'ita', 'jpn', 'nld', 'por', 'rus', 'spa'];  /* creating an array of translation keys */
+const keys1 = keys.toString().toLowerCase();        /*Since .includes() is key sensitive, I had to use .toString() and .toLowerCase();  */
+
+console.log("Supported translation keys are: \n" + keys);
+console.log("\n");
+
+/*creating an interface for user's input. */
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-    rl.question('Please enter the translation key:  ', (key) => {
-        if (keys1.includes(key)){
-            console.log(data.map(point=>point.translations[key]));
-        }
+/*Requesting User to enter the translation key. If Key is supported- program will print every country from .json file with specific translation key.*/
+
+rl.question('Please enter the translation key to see countries names:  ', (key) => {
+    if (keys1.includes(key)) {             /*Checking if entered translation key is supported and exsists in .json file.*/
+        console.log(data.map(point => point.translations[key].official));        /*printing every country name depend on the entered translation key. */
+    }
     else {
         console.log("Translation key is not supported. ");
     }
-  rl.close();
+    rl.close();
 });
-(${answer})){ 
-    
-    console.log(data.map(point=>point.translations.deu.official));  /* printing official name of the country depending on the translation key inserted 
-}
-else{ 
-    console.log("Translation key is not supported");
-} 
-*/
