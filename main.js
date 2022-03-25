e4a83344ca7d7646216d6d94c03ec358e3b57130
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-03-25 17:55:52 +0400

    Only official names are printed now.
    Added .official to return only official names of the countries.

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const data = require('/home/farkhad/progs/JS/countries.json');          /* Passi
 var keys = ['cym', 'deu', 'fra', 'hrv', 'ita', 'jpn', 'nld', 'por', 'rus', 'spa'];  /* creating an array of translation keys */
 const keys1 = keys.toString().toLowerCase();        /*Since .includes() is key sensitive, I had to use .toString() and .toLowerCase();  */
 
+console.log("Supported translation keys are: \n" + keys);
+console.log("\n");
+
 /*creating an interface for user's input. */
 const readline = require('readline');
 const rl = readline.createInterface({
@@ -10,12 +13,11 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
 /*Requesting User to enter the translation key. If Key is supported- program will print every country from .json file with specific translation key.*/
 
-rl.question('Please enter the translation key:  ', (key) => {
+rl.question('Please enter the translation key to see countries names:  ', (key) => {
     if (keys1.includes(key)) {             /*Checking if entered translation key is supported and exsists in .json file.*/
-        console.log(data.map(point => point.translations[key]));        /*printing every country name depend on the entered translation key. */
+        console.log(data.map(point => point.translations[key].official));        /*printing every country name depend on the entered translation key. */
     }
     else {
         console.log("Translation key is not supported. ");
