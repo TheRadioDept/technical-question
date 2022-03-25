eaf269c581c1288983872a409f68c7a2d1eb7095
Farkhad <farkhadmaratov99@gmail.com>
2022-03-25 17:27:20 +0400

    Problem solved.
    since .includes() is key sensitive, I have added toLowerCase function.

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 const data = require('/home/farkhad/progs/JS/countries.json'); /* Passing a .json file into javascript*/
- 
- var keys = ['cym','deu','fra','hrv','ita','jpn','nld','por','rus','spa'];  /* creating an array of translation keys */
 
+var keys = ['cym','deu','fra','hrv','ita','jpn','nld','por','rus','spa'];  /* creating an array of translation keys */
+const keys1 = keys.toString().toLowerCase();
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-
-    rl.question('Please enter the translation key ', (key) => {
-        if (keys.includes(key)){
-            console.log(data.map(point=>point.translations.key));
+    rl.question('Please enter the translation key:  ', (key) => {
+        if (keys1.includes(key)){
+            console.log(data.map(point=>point.translations[key]));
         }
     else {
         console.log("Translation key is not supported. ");
     }
   rl.close();
 });
-
-/*if(keys.includes(${answer})){ 
+(${answer})){ 
     
     console.log(data.map(point=>point.translations.deu.official));  /* printing official name of the country depending on the translation key inserted 
 }
