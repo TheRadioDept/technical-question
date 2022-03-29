4953fb8ad40501cc9f9c00daf6ab895ec31a14e9
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-03-29 20:19:44 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
   'env': {
-    'browser': true,
+    'node': true,
     'commonjs': true,
     'es2021': true,
   },
-  'extends': [
-    'eslint:recommended', 'google'
-  ],
   'parserOptions': {
     'ecmaVersion': 'latest',
   },
